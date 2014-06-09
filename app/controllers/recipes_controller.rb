eb5f66eb3d52096538398e51aa9b0e3b1540ad6d
Hawley Waldman <hawleyw@gmail.com>
2014-06-09 11:56:55 -0400

    Redirect users back to recipe listing page if they try to go to a recipe that does not exist.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,9 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show,
+                                     :edit,
+                                     :update,
+                                     :destroy]
+
   def index
     @recipes = Recipe.all
   end
@@ -21,15 +26,15 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+    # before_action @recipe = Recipe.find(params[:id])
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
+    # before_action @recipe = Recipe.find(params[:id])
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
+    # before_action @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
       flash[:notice] = "Recipe has been updated."
       redirect_to @recipe
@@ -40,7 +45,7 @@ class RecipesController < ApplicationController
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
+    # before_action @recipe = Recipe.find(params[:id])
     @recipe.destroy
     flash[:notice] = "Recipe has been deleted."
     redirect_to recipes_path
@@ -49,5 +54,12 @@ class RecipesController < ApplicationController
   private
     def recipe_params
       params.require(:recipe).permit(:name, :instructions, :ingredients)
+    end
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert]= "The recipe you were looking for could not be found."
+      redirect_to recipes_path
     end
 end
